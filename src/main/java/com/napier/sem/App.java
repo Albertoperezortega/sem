da6ae067b82cb701a05322b057b14b6abe256961
arielgirs <40495923@live.napier.ac.uk>
2022-03-03 20:02:31 +0000

    Tested SQL queries. Database responsive

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -6,6 +6,30 @@ public class App
 {
     public static void main(String[] args)
     {
+        // Create new Application
+        App a = new App();
+
+        // Connect to database
+        a.connect();
+        // Get Employee
+        Country ctr = a.getCountry();
+        // Display results
+        a.displayCountry(ctr);
+
+        // Disconnect from database
+        a.disconnect();
+    }
+
+    /**
+     * Connection to MySQL database.
+     */
+    private Connection con = null;
+
+    /**
+     * Connect to the MySQL database.
+     */
+    public void connect()
+    {
         try
         {
             // Load Database driver
@@ -17,9 +41,7 @@ public class App
             System.exit(-1);
         }
 
-        // Connection to the database
-        Connection con = null;
-        int retries = 100;
+        int retries = 10;
         for (int i = 0; i < retries; ++i)
         {
             System.out.println("Connecting to database...");
@@ -30,9 +52,6 @@ public class App
                 // Connect to database
                 con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
                 System.out.println("Successfully connected");
-                // Wait a bit
-                Thread.sleep(10000);
-                // Exit for loop
                 break;
             }
             catch (SQLException sqle)
@@ -45,7 +64,62 @@ public class App
                 System.out.println("Thread interrupted? Should not happen.");
             }
         }
+    }
 
+
+    public Country getCountry()
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT code, name, continent "
+                            + "FROM country "
+                            + "WHERE code = 'AGO'";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Return new country if valid.
+            // Check one is returned
+            if (rset.next())
+            {
+                Country ctr = new Country();
+                ctr.code = rset.getString("code");
+                ctr.name = rset.getString("name");
+                ctr.continent = rset.getString("continent");
+                return ctr;
+            }
+            else
+                return null;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get country details");
+            return null;
+        }
+    }
+
+    public void displayCountry(Country ctr)
+    {
+        if (ctr != null)
+        {
+            System.out.println(
+                    ctr.code + " "
+                            + ctr.name + " "
+                            + ctr.continent+ "\n");
+        }
+    }
+
+
+
+
+    /**
+     * Disconnect from the MySQL database.
+     */
+    public void disconnect()
+    {
         if (con != null)
         {
             try
@@ -59,4 +133,5 @@ public class App
             }
         }
     }
+
 }
---
+++ src/main/java/com/napier/sem/Country.java
@@ -0,0 +1,83 @@
+package com.napier.sem;
+
+/**
+ *
+ * Represents a country
+     */
+    public class Country
+    {
+        /**
+         * Country 3 letter code
+         */
+        public String code;
+
+        /**
+         * Country's name
+         */
+        public String name;
+
+        /**
+         * Name of continent
+         */
+        public String continent;
+
+        /**
+         * Name of the region
+         */
+        public String region;
+
+        /**
+         * Surface area of country
+         */
+        public float surface_area;
+
+        /**
+         * Independence year of the country
+         */
+        public int indep_year;
+
+        /**
+         * Population of the country
+         */
+        public int population;
+
+        /**
+         * Life expectency of the country's citizens
+         */
+        public float life_expectancy;
+
+        /**
+         * Gross national product of the country
+         */
+        public float gnp;
+
+        /**
+         * Old gross national product of the country
+         */
+        public float gnp_old;
+
+        /**
+         * Name of the country in their own language
+         */
+        public String local_name;
+
+        /**
+         * Government form: republic etc...
+         */
+        public String government_form;
+
+        /**
+         * Head of the country
+         */
+        public String head_of_state;
+
+        /**
+         * Capital of the country
+         */
+        public int capital;
+
+        /**
+         * Code2 of the country
+         */
+        public String code2;
+    }
